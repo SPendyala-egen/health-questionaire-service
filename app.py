8715dc61bf5ed4acdf8afb1288867f5b68a6b0ac
Erwin Palani <erwinpalani@Erwins-MacBook-Pro.local>
2023-06-19 17:17:42 -0500

    add singleton map to track and group queries

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 from geopy.geocoders import Nominatim
 import openai
 import config
@@ -9,10 +9,27 @@ openai.api_key = config.OPENAI_API_KEY
 ipstack_api_key = config.IPSTACK_API_KEY
 geolocator = Nominatim(user_agent='myGeocoder')  
 
-
-# Continue with your Flask app code
-
 app = Flask(__name__)
+
+class SingletonMap:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.map = {}
+
+        return cls._instance
+
+    def add_item(self, key, value):
+        self.map[key] = value
+
+    def get_item(self, key, default=None):
+        return self.map.get(key, default)
+
+    def print_map(self):
+        for key, value in self.map.items():
+            print(key, value)
 
 def generate_chat_response(user_input):
     # Call the OpenAI API to generate a response
@@ -26,13 +43,23 @@ def generate_chat_response(user_input):
     )
     return response.choices[0].text.strip()
 
+@app.route('/')
+def index():
+    singleton_map = SingletonMap()
+
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.json['message']
     user_ip_address = request.json['ipAddress']
     chat_gpt_request = construct_request(user_input, user_ip_address)
+    record_query(chat_gpt_request)
     response = generate_chat_response(chat_gpt_request)
-    return jsonify({'message': response})
+    return jsonify({'response': response})
+
+@app.route('/stats', methods=['GET'])
+def get_stats():
+    singleton_map = SingletonMap()
+    return jsonify(dict(singleton_map.map.items()))
 
 def get_lat_long_from_ip(ip_address):
 
@@ -58,6 +85,14 @@ def construct_request(message, ip_address):
         return f'{message} near {address}'
     else:
         return message
+
+def record_query(query):
+    singleton_map = SingletonMap()
+    print(query)
+    if singleton_map.get_item(query) == 1:
+        singleton_map.add_item(query, singleton_map.get_item(query) + 1)
+    else:
+        singleton_map.add_item(query, 1)
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
